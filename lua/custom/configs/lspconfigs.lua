fcfe469951968c4eb91b0d665e36e9db5d8204b4
Randell <piszmogcode@gmail.com>
2023-07-02 21:48:30 -0600

    add plugins

--- lua/custom/configs/lspconfigs.lua
+++ lua/custom/configs/lspconfigs.lua
@@ -1,34 +1,44 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
-lspconfig.gopls.setup({
+lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+    },
+  },
+}
 
-lspconfig.svelte.setup({
+lspconfig.svelte.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+}
 
-lspconfig.tailwindcss.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
-})
-
-lspconfig.html.setup({
+lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+}
 
-lspconfig.tsserver.setup({
+lspconfig.html.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+}
 
-lspconfig.eslint.setup({
+lspconfig.tsserver.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+}
+
+lspconfig.eslint.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -11,57 +11,77 @@ return {
         "html-lsp",
         "prettier",
         "typescript-language-server",
-        "eslint-lsp"
-      }
-    }
+        "eslint-lsp",
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
       "jose-elias-alvarez/null-ls.nvim",
-      config = function ()
+      config = function()
         require "custom.configs.nullls"
-      end
+      end,
     },
-    config = function ()
+    config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfigs"
-    end
+    end,
   },
   {
     "rust-lang/rust.vim",
     ft = "rust",
-    init = function ()
+    init = function()
       vim.g.rustfmt_autosave = 1
-    end
+    end,
   },
   {
     "simrat39/rust-tools.nvim",
     ft = "rust",
     dependencies = "neovim/nvim-lspconfig",
-    opts = function ()
+    opts = function()
       return require "custom.configs.rusttools"
     end,
-    config = function (_, opts)
-      require('rust-tools').setup(opts)
-    end
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
   },
   {
     "saecki/crates.nvim",
-    ft = {"rust", "toml"},
+    ft = { "rust", "toml" },
     dependencies = "hrsh7th/nvim-cmp",
-    opts = function (_, opts)
-      local crates = require("crates")
+    opts = function(_, opts)
+      local crates = require "crates"
       crates.setup(opts)
       crates.show()
-    end
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
-    opts = function ()
+    opts = function()
       local M = require "plugins.configs.cmp"
-      table.insert(M.sources, {name = "crates"})
+      table.insert(M.sources, { name = "crates" })
       return M
-    end
-  }
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    opts = function()
+      return require "custom.configs.copil"
+    end,
+    config = function(_, opts)
+      require("copilot").setup(opts)
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
